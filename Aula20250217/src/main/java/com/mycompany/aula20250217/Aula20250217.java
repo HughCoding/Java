4f48d68f8be0628992f79a8eca71f73b7e997159
HughCoding <160680383+HughCoding@users.noreply.github.com>
2025-02-20 10:09:39 -0300

    Update Aula20250217.java

--- Aula20250217/src/main/java/com/mycompany/aula20250217/Aula20250217.java
+++ Aula20250217/src/main/java/com/mycompany/aula20250217/Aula20250217.java
@@ -1,13 +1,13 @@
-package com.mycompany.aula20250217;
-import java.io.IOException;
+package com.mycompany.aula20250220;
+
 import java.util.Scanner;
 
-public class Aula20250217 {
+public class Aula20250220 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         
         Scanner scanner = new Scanner(System.in);
-        int opcao = 5;
+        int opcao;
         int resultado = 0;
         
         while(true){
@@ -22,12 +22,14 @@ public class Aula20250217 {
         System.out.printf("=======================================\n");
         System.out.printf("Escolha uma opção: ");
             
-     
-        opcao = scanner.nextInt();
+         opcao = scanner.nextInt();
+         
+         if(opcao == 0){
+             System.out.println("Saindo da calculadora... ");
+             break;
+         }
         
        switch(opcao){
-           case 0:
-            System.out.println("Saindo da calculadora... ");
            case 1:
             System.out.println("Opção válida! Você escolheu a opção: " + opcao);
             break;
@@ -46,31 +48,28 @@ public class Aula20250217 {
                    
        }
         
-       System.out.println("Escolha o primeiro número:");
+       System.out.print("Escolha o primeiro número:");
        int num1 = scanner.nextInt();
-       System.out.println("Escolha o segundo número:");
+       System.out.print("Escolha o segundo número:");
        int num2 = scanner.nextInt();
       
        
-       
-       
-       if(opcao == 1) {
-           resultado = num1 + num2;
-       } else if(opcao == 2) {
-           resultado = num1 - num2;
-       } else if(opcao == 3) {
-           resultado = num1 * num2;
-       } else if(opcao == 4){ 
-           if (num2 != 0) {
-           resultado = num1 / num2;
-       } else{
-           System.out.println("Numero Invalido");
-       }
-       }
+            switch (opcao) {
+                case 1 -> resultado = num1 + num2;
+                case 2 -> resultado = num1 - num2;
+                case 3 -> resultado = num1 * num2;
+                case 4 -> {
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else{
+                        System.out.println("Numero Invalido");
+                    }
+                }
+                default -> {
+                }
+            }
         System.out.println("Resultado:" + resultado);
-
-        
     }
-   
+  
 }
 }
