829cf94104dd900d74ad1c1d5c1823fc3af64842
HughCoding <160680383+HughCoding@users.noreply.github.com>
2025-03-23 22:41:42 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Motorista m1 = new Motorista("Carlos", "313.122.233-21");
-
-        Motor motor1 = new Motor(300, "Aditivada");
-
-        Carro c1 = new Carro("BMW", m1, motor1);
+        Carro c1 = new Carro("BMW", m1, 200);
 
         System.out.println(c1);
 
